0e307cd4a7341f6478a33b547362a85731fb8d63
Daniel Filho <daniel.acfilho@gmail.com>
2014-09-22 17:16:46 -0300

    Remove extra options

--- spec/javascripts/eventTrackerSpec.js
+++ spec/javascripts/eventTrackerSpec.js
@@ -1,17 +1,13 @@
 describe("eventTracker", function() {
 
-  describe("Initial plugin setup", function() {
+  describe("Plugin setup", function() {
     it("should expose the 'eventTracker'", function() {
       expect($.fn.eventTracker).toBeDefined();
     });
 
-    it("should permits extra params", function() {
-      $("body").eventTracker({extra: "test"});
-
-      expect($("body").data("eventTracker").options.extra).toEqual("test");
-    });
-
     it("should configure the element data", function() {
+      $("body").eventTracker();
+
       expect($("body").data("eventTracker").element).toEqual($("body")[0]);
     });
   });
